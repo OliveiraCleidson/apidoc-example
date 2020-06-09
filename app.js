7933b75a07de7dd9229e42915b7a23c552677c6e
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2020-06-09 17:20:37 -0300

    new version 0.0.2

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ var app = express();
 app.use(express.static("public"));
 
 /**
- * @api {get} /signin Singin
- * @apiVersion 0.0.1
+ * @api {get} /signin Sign In
+ * @apiVersion 0.0.2
  * @apiGroup Autenticação
- *
+ * @apiDescription API de Sign In de usuário
+ * 
+ * @apiHeader {string} user Nome do Usuário
+ * @apiHeader {string} hashPass Senha Criptografada
+ * @apiHeader {Number} [keep=0] Se mantém logado
+ * 
+ * @apiHeaderExample {json} Request-Example:
+ * {
+ *    "user": "gk.cleidson@yahoo.com.br",
+ *    "hashPass": "402562bbebdabd3dc1c86ad268ab3136aebd1756",
+ *     "keep": 1
+ * }
  * @apiSuccess {String} status Mensagem de acesso autorizado
+ * @apiSuccess {String} token Token de Acesso
  * 
  * @apiSuccessExample {json} Sucesso
  *    HTTP/1.1 200 OK
  *    {
  *      "status": "Logado!"
+ *      "token": "c447c5b517efa57e04e1a4732b2a3b5df82f9d72"
+ *    }
+ * 
+ * @apiError {String} status Mensagem de acesso negada
+ * 
+ * @apiErrorExample {json} Error
+ *    HTTP/1.1 406 Not Acceptable
+ *    {
+ *      "status": "Usuário ou Senha incorretos"
  *    }
  *
  */
@@ -22,16 +43,39 @@ app.get("/signin", function (req, res) {
 });
 
 /**
- * @apiVersion 0.0.1
- * @api {post} /signup Signup
+ * @apiVersion 0.0.2
+ * @api {post} /signup Sign Up
  * @apiGroup Autenticação
+ * @apiDescription API de cadastro de novo usuário
  *
+ * @apiHeader {string} user Nome do Usuário
+ * @apiHeader {string} hashPass Senha Criptografada
+ * 
+ * @apiHeaderExample {json} Request-Example:
+ * {
+ *    "user": "gk.cleidson@yahoo.com.br",
+ *    "hashPass": "402562bbebdabd3dc1c86ad268ab3136aebd1756",
+ * }
+ * 
  * @apiSuccess {String} status Mensagem de cadastro efetuado
  * 
  * @apiSuccessExample {json} Sucesso
- *    HTTP/1.1 200 OK
+ *    HTTP/1.1 201 Created
  *    {
- *      "status": "Cadastrado!"
+ *      "status": "Usuário Cadastrado!"
+ *    }
+ * 
+ * @apiError {String} status Mensagem de Cadastro Não Efetivado 
+ * 
+ * @apiErrorExample {json} Email em Uso
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "status": "Email já utilizado"
+ *    }
+ *  @apiErrorExample {json} Falta Campo hashPass ou user
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "status": "Faltam Argumentos"
  *    }
  *
  */
